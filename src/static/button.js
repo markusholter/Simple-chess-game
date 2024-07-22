d367532fe0dd978ff32fe33d0e4dd762dc8e1792
Markus Holter <markus.holter@hotmail.com>
2024-07-22 15:10:32 +0200

    Change to english

---
+++ src/static/button.js
@@ -0,0 +1,5 @@
+let socket = io.connect("ws://127.0.0.1:5000")
+
+document.getElementById("button").onclick = function() {
+    socket.emit("message", "Hello")
+}
--- src/static/knapp.js
+++
@@ -1,5 +0,0 @@
-let socket = io.connect("ws://127.0.0.1:5000")
-
-document.getElementById("knapp").onclick = function() {
-    socket.emit("message", "heii")
-}

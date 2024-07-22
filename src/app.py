e3fe5b906f466642377155551721a018f474f2f8
Markus Holter <markus.holter@hotmail.com>
2024-07-22 15:52:23 +0200

    Add functionality to send message to client

--- src/app.py
+++ src/app.py
@@ -9,9 +9,18 @@ socket = SocketIO(app)
 def index():
     return render_template("index.html")
 
+@socket.on("connect")
+def handle_connect():
+    app.logger.info("Client connected")
+
 @socket.on("message")
 def handle_message(data):
     app.logger.info("The data: " + data)
+
+@app.route("/send")
+def send_new():
+    socket.emit("message", "New hello world!")
+    return "Success!"
 
 if __name__ == "__main__":
     socket.run(app, debug=True)
--- src/static/button.js
+++ src/static/button.js
@@ -3,3 +3,7 @@ let socket = io.connect("ws://127.0.0.1:5000")
 document.getElementById("button").onclick = function() {
     socket.emit("message", "Hello")
 }
+
+socket.on("message", function(data)  {
+    document.getElementById("world").innerHTML = data
+}) 

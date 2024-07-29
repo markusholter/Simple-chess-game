317fd85d1861ff271c2dfffabfec9a41cf012be5
Markus Holter <markus.holter@hotmail.com>
2024-07-29 15:27:01 +0200

    Add rules for knight

--- src/objects/pieces/Knight.py
+++ src/objects/pieces/Knight.py
@@ -3,3 +3,9 @@ from ..pieces.Piece import Piece
 class Knight(Piece):
     def __init__(self, white: bool, image: str) -> None:
         super().__init__(white, image)
+
+    def turn(self, start, end, board):
+        diffx = abs(end[0] - start[0])
+        diffy = abs(end[1] - start[1])
+
+        return diffx + diffy == 3
